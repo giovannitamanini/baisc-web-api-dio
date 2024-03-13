006558c0ac1aa0f4d2cbbfdeb637ffc99c3ba4ed
Giovanni Tamanini <giovanni.tnini@gmail.com>
2024-03-13 14:47:49 -0300

    Documentando WelcomeController

--- src/main/java/dio/web/api/controller/WelcomeController.java
+++ src/main/java/dio/web/api/controller/WelcomeController.java
@@ -1,11 +1,13 @@
 package dio.web.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Operation(summary = "Welcome page")
     @GetMapping
     public String welcome() {
         return "Welcome to my Spring Boot Web API";
